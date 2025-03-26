4a69c856717919bfe727942c8f3135664cd5c598
shoce <yogze@icloud.com>
2025-03-26 14:40:42 +0530

    days

--- days/days.go
+++ days/days.go
@@ -1,7 +1,7 @@
 /*
 GoFmt
-GoRun
 GoBuild
+GoRun
 */
 
 package main
@@ -9,14 +9,21 @@ package main
 import (
 	"fmt"
 	"time"
+	"strings"
 )
 
-const Cycle = 28
+const (
+	Cycle = 28
+	SEP = " ; "
+	DATESEP = "/"
+	TodayPrefix = "* "
+	TodaySuffix = " *"
+)
 
 func dayfmt(td time.Duration) string {
 	days := int(td / (time.Duration(24) * time.Hour))
 	//return fmt.Sprintf("%d", days+1)
-	return fmt.Sprintf("%d/%d", (days/Cycle)+1, (days%Cycle)+1)
+	return fmt.Sprintf("%d"+DATESEP+"%d", (days/Cycle)+1, (days%Cycle)+1)
 }
 
 func main() {
@@ -24,13 +31,21 @@ func main() {
 	ty0 := time.Date(tnow.Year(), 1, 1, 0, 0, 0, 0, time.Local)
 	ty1 := time.Date(tnow.Year()+1, 1, 1, 0, 0, 0, 0, time.Local)
 
-	var prefix string
 	for t := ty0; t.Before(ty1); t = t.Add(time.Duration(24) * time.Hour) {
-		if tnow.After(t) && tnow.Before(t.Add(time.Duration(24)*time.Hour)) {
-			prefix = ">"
-		} else {
-			prefix = " "
+		if t.Sub(ty0) % (Cycle * time.Duration(24) * time.Hour) == 0 {
+			fmt.Println()
 		}
-		fmt.Println(prefix, dayfmt(t.Sub(ty0)), t.Format("Jan/2"))
+		today := false
+		if tnow.After(t) && tnow.Before(t.Add(time.Duration(24)*time.Hour)) {
+			today = true
+		}
+		days := dayfmt(t.Sub(ty0))
+		canondate := strings.ToLower(t.Format("Jan"+DATESEP+"2"))
+		date := days + " " + canondate
+		if today {
+			date = TodayPrefix+date+TodaySuffix
+		}
+		fmt.Print(date+SEP)
 	}
+	fmt.Println()
 }
