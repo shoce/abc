b803ae8fde3daf939521d9155c02960360232c7f
shoce <yogze@icloud.com>
2025-11-28 16:47:04 +0700

    svstat

--- svstat/svstat.go
+++ svstat/svstat.go
@@ -184,7 +184,7 @@ func main() {
 			fmt.Fprintf(os.Stderr, "ERROR Service.ReadStatus %s\n", err)
 			os.Exit(1)
 		}
-		//fmt.Printf("%s %s pid<%d> <%ds>, %s\n", s.Path, s.Status, s.PID, s.Seconds, s.Action)
-		fmt.Printf("path %s status %s pid <%d> seconds <%ds> action %s"+NL, s.Path, s.Status, s.PID, s.Seconds, s.Action)
+		//fmt.Printf("%s: %s pid<%d> <%ds>, %s\n", s.Path, s.Status, s.PID, s.Seconds, s.Action)
+		fmt.Printf("path[%s] status[%s] pid<%d> seconds<%ds> action[%s]"+NL, s.Path, s.Status, s.PID, s.Seconds, s.Action)
 	}
 }
